77d59668d468c15af93b4981663100d393a38c1f
gageracer <gageracer@gmail.com>
2025-01-15 16:51:32 -0800

    serviceworker

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		version: {
+			// ideally, this should be something deterministic
+			// like the output of `git rev-parse HEAD`
+			name: Date.now().toString(),
+
+			// if undefined, no polling will occur
+			pollInterval: 5000
+		}
 	}
 };
 
