f4ef4e67064e46835fbc1f7e3c523930c24eda73
gageracer <gageracer@gmail.com>
2025-08-10 22:27:07 -0700

    kit config restored

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,20 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+			adapter: adapter(),
+			version: {
+				// ideally, this should be something deterministic
+				// like the output of `git rev-parse HEAD`
+				name: Date.now().toString(),
+
+				// if undefined, no polling will occur
+				pollInterval: 30000
+			}
+		}
 };
 
 export default config;
